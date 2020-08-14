8649ffb2ed4b095febb570e772fc5baea420ef6e
Vasilios Kambouras <vasilioskw@gmail.com>
2020-08-14 21:37:18 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -10,7 +10,7 @@ def fizzbuzz(x)
       
    else if (x / 3).class == Integer && (x / 5).class == Integer
   puts "fizzBuzz"
-   else if  (x / 3).class && (x / 5).class != Integer
+   else if  (x / 3).class == Integer && && (x / 5).class != Integer
     puts nil
 end
     
