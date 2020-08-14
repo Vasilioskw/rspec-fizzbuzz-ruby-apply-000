80c49e320b6190817d70f3a5cef3ab0f4575d526
Vasilios Kambouras <vasilioskw@gmail.com>
2020-08-14 21:24:54 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,25 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+
+fizzbuzz(x)
+x / 3 == Integer
+  puts "fizz"
+    else if x / 3 !=  Integer
+      puts "nil" 
+end
+    
+fizzbuzz(x)
+x / 5 == Integer
+    puts "Buzz"
+    else if x != Integer
+      puts "nil"
+end
+
+fizzbuzz(x)
+ x / 3 && x / 5 == Integer
+  puts "fizzBuzz"
+   else if  x / 3 && x / 5 != Integer
+    puts nil
+end
+    
