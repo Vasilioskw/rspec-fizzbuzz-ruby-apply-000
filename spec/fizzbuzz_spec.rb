5f2acd11130520d5ae0e5bc3d1f3203f733991b6
Vasilios Kambouras <vasilioskw@gmail.com>
2020-08-14 21:51:35 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,8 +4,8 @@ require_relative '../fizzbuzz.rb'
 
 describe "fizzbuzz" do
   it 'returns "Fizz" when the number is divisible by 3' do
-   fizzbuzz_3 = fizzbuzz(3)
-        expect(fizz_3).to eq("Fizz")
+   fizz_3 = fizzbuzz(3)
+        expect(fizzbuzz_3).to eq("Fizz")
       end 
         it 'returns "Fizz" when the number is not divisible by 3' do
         fizz_4 = fizz(4)
