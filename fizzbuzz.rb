e278b15a7fea4c6b0c658263423c281bde4b47e1
Vasilios Kambouras <vasilioskw@gmail.com>
2020-08-14 21:43:08 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,8 @@
-# Don't forgdef fizzbuzz(x)
+# Don't forget! This file needs to be 'required' in its spec file
+# See README.md for instructions on how to do this
+
+
+def fizz(x)
    if (x / 3).class == Integer && (x / 5).class == Integer
   return "FizzBuzz"
   
@@ -9,7 +13,4 @@
   
 end
 return nil
-endet! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
-
-
+end
